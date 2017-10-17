d147826b3e1d7df70412d46986882d4669054178
Sam Luby <samwluby@gmail.com>
2017-10-17 16:43:23 +0100

    Fixes

--- src/ie/ucd/cluedo/SuspectCards.java
+++ src/ie/ucd/cluedo/SuspectCards.java
@@ -1,21 +1,15 @@
 package ie.ucd.cluedo;
 
 public class SuspectCards extends Cards {
-
-	private static String[] suspects = {"Miss Scarlett", "Professor Plum", "Mrs. Peacock", "Reverend Mr Green", "Colonel Mustard", "Mrs White"};
-	
+	private static String[] names = {"Miss Scarlett", "Professor Plum", "Mrs. Peacock", "Reverend Mr Green", "Colonel Mustard", "Mrs White"};
 	
 	public SuspectCards() {
-		String[] names = suspects;
 	}
 	
-	
 	public static void main(String[] args) {
-		SuspectCards s = new SuspectCards();
-		String[] x = s.suspects;
 		
-		for(int i=0; i < x.length; i++) {
-			System.out.println(suspects[i]);
+		for(int i=0; i < names.length; i++) {
+			System.out.println(names[i]);
 		}
 	}
 }
