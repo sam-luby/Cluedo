2c80bd6069e545c8d7a31135695cc0614766d447
Sam Luby <samwluby@gmail.com>
2017-11-05 17:45:49 +0000

    More code cleanup

--- src/ie/ucd/cluedo/Main.java
+++ src/ie/ucd/cluedo/Main.java
@@ -12,45 +12,19 @@ import ie.ucd.cluedo.enums.SuspectCards;
 
 
 public class Main {
-	
 	public static void main(String[] args) {
 			
 		
 		PlayerSetup setupPlayers = new PlayerSetup();
 		setupPlayers.addPlayers();
-		String[] names = setupPlayers.getPlayers();
 		ArrayList<Player> players = new ArrayList<Player>();
-		
-		
-//		setupPlayers.setPawns(names);
-//		Map<String, String> playerPawnAssignment = setupPlayers.getPawns();
-//		
-//		Set<?> set = playerPawnAssignment.entrySet();
-//		Iterator<?> iter = set.iterator();
-//		
-//		while(iter.hasNext()) {
-//			Map.Entry me = (Map.Entry)iter.next();
-//			System.out.print(me.getKey() + ": ");
-//			System.out.println(me.getValue());
-//		}
-//		
-//		SuspectCards suspects;
-//		AllCards cards;
-//		
-//		ArrayList<Player> playerNames = setupPlayers.setPlayers();
-//		
-//		for(Player p : playerNames) {
-//			System.out.println(p.getName());
-//			System.out.println(p.getPawn());
-//			System.out.println(p.getCards());
-//		}
+
 		
 		players = setupPlayers.setPlayers();
 		for(Player p : players) {
-			System.out.println(p.getName() + " " +  p.getPawn());
-			System.out.println(p.getCards());
+			System.out.println("Player: " + p.getName() + ", Pawn: " +  p.getPawn());
+			System.out.println("Cards: " + p.getCards());
 		}
 
-		
 	}
 }
