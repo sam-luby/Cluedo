6b2e307bd4fd7e200fa894cc73ca47325c258955
DarrenCoughlan <darren.coughlan@ucdconnect.ie>
2017-11-16 23:37:37 +0000

    added initialise board method

--- src/ie/ucd/cluedo/CluedoBoard.java
+++ src/ie/ucd/cluedo/CluedoBoard.java
@@ -12,11 +12,14 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 
+import ie.ucd.cluedo.enums.SuspectCards;
+
 public class CluedoBoard {
 
 	private int boardWidth;
 	private int boardHeight;
 	private char[][] inputBoard;
+	private char[][] initialiseBoard;
 	File boardTextFile = new File("boardNew.txt");
 
 	public CluedoBoard() throws FileNotFoundException {
@@ -32,16 +35,42 @@ public class CluedoBoard {
 //		System.out.println(boardWidth);
 		
 		inputBoard = new char[boardWidth][boardHeight];
+		initialiseBoard = new char[boardWidth][boardHeight];
 		Scanner scanner = new Scanner(boardTextFile);
 		for (int row = 0; scanner.hasNextLine() && row < boardWidth; row++) {
 			inputBoard[row] = scanner.nextLine().toCharArray();
 		}
 		scanner.close();
 	}
+	
+	private void initialiseBoard(int numPlayers, String[] names) {
+		int x = 0;
+		for (int i = 0; i < boardWidth; i++) {
+			for (int j = 0; j < boardHeight; j++) {
+				if(numPlayers != 0 && inputBoard[i][j] == 'S') {
+					initialiseBoard[i][j] = Character.toUpperCase(names[x].charAt(0));
+					numPlayers--;
+					x++;
+				}	
+				else if(numPlayers == 0 && inputBoard[i][j] == 'S') {
+					initialiseBoard[i][j] = ' ';
+				}
+				else {
+					initialiseBoard[i][j] = inputBoard[i][j];
+				}
+			}
+		}
+		for (int i = 0; i < boardWidth; i++) {
+			for (int j = 0; j < initialiseBoard[i].length; j++) {
+				System.out.print(initialiseBoard[i][j]);
+			}
+			System.out.println();
+		}		
+	}
 
 	private void printBoard() {
 		for (int i = 0; i < boardWidth; i++) {
-			for (int j = 0; j < inputBoard[i].length; j++) {
+			for (int j = 0; j < boardHeight; j++) {
 				System.out.print(inputBoard[i][j]);
 			}
 			System.out.println();
@@ -49,7 +78,13 @@ public class CluedoBoard {
 	}
 
 	public static void main(String[] args) throws IOException {
+		PlayerSetup setupPlayers = new PlayerSetup();
+		ArrayList<Player> players = new ArrayList<Player>();
+		players = setupPlayers.setupPlayers();
+		
 		CluedoBoard myBoard = new CluedoBoard();
+		myBoard.initialiseBoard(players.size(), setupPlayers.getPlayers());
+		System.out.println();
 		myBoard.printBoard();
 	}
 
