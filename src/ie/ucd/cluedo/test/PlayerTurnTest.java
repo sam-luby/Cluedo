d4a1345457ad00da3a698384227dda3055e0fe79
Sam Luby <samwluby@gmail.com>
2017-11-24 17:16:55 +0000

    Added a test class

---
+++ src/ie/ucd/cluedo/test/PlayerTurnTest.java
@@ -0,0 +1,20 @@
+package ie.ucd.cluedo.test;
+
+import static org.junit.Assert.*;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import ie.ucd.cluedo.Player;
+import ie.ucd.cluedo.PlayerTurn;
+
+public class PlayerTurnTest {
+
+//	ArrayList<String> cards = new ArrayList<String>(Arrays.asList("Card1", "Card2", "Card3"));
+//	Player sam = new Player("Sam", "Reverend Green", cards);
+//	int[] location = {5,5};
+//	PlayerTurn myTurn = new PlayerTurn(sam, location);
+}
